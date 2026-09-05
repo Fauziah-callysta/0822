871e09a7140cac82653c1e436c9e0acad31416d6
Fauziah Callysta <fauziahcallysta2907@gmail.com>
2026-09-05 21:47:51 +0700

    fix routes

--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+import re
 
-def rupiah(amount):
-    """
-    Format angka int menjadi string Rupiah
-    contoh 15000 -> Rp15.000
-    """
+
+def slugify(text: str) -> str:
+    text = text.strip().lower()
+    text = re.sub(r"[^a-z0-9]+", "-", text)
+    return text.strip("-")
+
+
+def rupiah(amount: int) -> str:
+    """Format angka int menjadi string Rupiah contoh 15000 -> Rp15.000"""
     return f"Rp{amount:,.0f}".replace(",", ".")
