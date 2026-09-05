8a93f3c06a9210d2a2ede41a08949795c157ab6b
Fauziah-callysta <fauziahcallysta2907@gmail.com>
2026-09-05 21:54:37 +0700

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,16 +1,24 @@
 from flask import Flask
-
-from utils import rupiah
+from utils import rupiah, slugify  # 1. Tambahkan import slugify di sini
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return "<h1>Hi..</h1>"
 
+
+# 2. Tambahkan route /slug/ di bawah ini
+@app.route("/slug/<teks>")
+def slug(teks):
+    return slugify(teks)
+
+
 @app.route("/to_rupiah/<int:amount>")
 def to_rupiah(amount):
     return rupiah(amount)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
